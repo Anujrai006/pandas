fbfe743174d5716f85a340ce82ce4d70f8f02109
Anuj Rai <anujxilling253745@gmail.com>
2026-03-18 06:06:30 +0545

    new file added and oandas imported

--- series.py
+++ series.py
@@ -1,1 +1,7 @@
+# In Pandas, a Series is a one-dimensional labeled array.
+# You can think of it like a single column of data (similar to a list, but with labels called index).
+import pandas as pd
 
+data = ["marks", 100, 99, 87]
+series = pd.Series(data, index=["name", "anuj", "amit", "rohan"])
+print(series)
