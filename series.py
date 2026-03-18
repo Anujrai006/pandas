6fc7665379d284e6b33ceed69108ec3fc00b6ea9
Anuj Rai <anujxilling253745@gmail.com>
2026-03-18 06:39:49 +0545

    new functions and learned to know series ,iloc,loc,filtering

--- series.py
+++ series.py
@@ -2,6 +2,15 @@
 # You can think of it like a single column of data (similar to a list, but with labels called index).
 import pandas as pd
 
-data = ["marks", 100, 99, 87]
-series = pd.Series(data, index=["name", "anuj", "amit", "rohan"])
-print(series)
+data = [100, 99, 87]
+series = pd.Series(data, index=[ "anuj", "amit", "rohan"])
+series.loc["rohan"]=99
+# print(series[series > 99])
+# print(series.loc["anuj"])
+calories={"Day-1":1200,"Day-2":1400,"Day-3":1250}
+series1=pd.Series(calories) 
+series1.loc["Day-2"]=1700
+
+print(series1.loc["Day-2"])
+print(series1[series1 < 1300])
+# print(series1)
