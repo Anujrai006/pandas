e05f3bb98ca39fade205a8ce5e03f278e80de3dc
Anuj Rai <anujxilling253745@gmail.com>
2026-03-22 05:23:59 +0545

    new file and some funtions of code updated

---
+++ SIde_pr.py
@@ -0,0 +1,3 @@
+n=2
+lst=[f"students{i}" for i in range(n)]
+print(lst)
--- project2.py
+++ project2.py
@@ -1,13 +1,9 @@
+#creating the data frame of students name and class . (list to updating into DF)
 import pandas  as pd
 import numpy as np
-data=[]
-students=[]
-
-        
-
-    
+students=[]  
 n=int(input("Enter the no of stds:"))
-for i in range(2):
+for i in range(n+1):
     name=input("Enter the name:")
     Class=int(input("enter the class"))
     # students["name"]=name
@@ -15,7 +11,7 @@ for i in range(2):
     data={"name":name,"class":Class}
     students.append(data)
 print(students)
-df=pd.DataFrame(students,index=[for i in range(n),f"students{i}"])
+df=pd.DataFrame(students,index=[f"student{i}:" for i in range(n+1)])
 print(df)
 
     # students.update(dict2)
