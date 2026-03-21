57f85077b6a12a81127008eaec1705bd66c5ec01
Anuj Rai <anujxilling253745@gmail.com>
2026-03-21 19:30:08 +0545

    .\project2.py

---
+++ project2.py
@@ -0,0 +1,26 @@
+import pandas  as pd
+import numpy as np
+data=[]
+students={}
+
+        
+
+    
+
+for i in range(2):
+    name=input("Enter the name:")
+    Class=int(input("enter the class"))
+    # students["name"]=name
+    # students["class"]=Class
+    dict1={"name":name,"class":Class}
+    dict2={}
+    dict2.update(dict1)
+    print(dict2)
+    # students.update(dict2)
+students.dict
+print(students)
+    
+    
+
+
+
