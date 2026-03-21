f4947d23b294cb713f99c65ce4dd9a9cfe3f3441
Anuj Rai <anujxilling253745@gmail.com>
2026-03-22 05:15:09 +0545

    new updates made

--- project2.py
+++ project2.py
@@ -1,24 +1,25 @@
 import pandas  as pd
 import numpy as np
 data=[]
-students={}
+students=[]
 
         
 
     
-
+n=int(input("Enter the no of stds:"))
 for i in range(2):
     name=input("Enter the name:")
     Class=int(input("enter the class"))
     # students["name"]=name
     # students["class"]=Class
-    dict1={"name":name,"class":Class}
-    dict2={}
-    dict2.update(dict1)
-    print(dict2)
-    # students.update(dict2)
-students.dict
+    data={"name":name,"class":Class}
+    students.append(data)
 print(students)
+df=pd.DataFrame(students,index=[for i in range(n),f"students{i}"])
+print(df)
+
+    # students.update(dict2)
+
     
     
 
