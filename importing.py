1b1e8b7700b9ea7128fa7d6fd6e51f66b4843ecc
Anuj Rai <anujxilling253745@gmail.com>
2026-03-23 05:12:43 +0545

    new updates

--- importing.py
+++ importing.py
@@ -1,11 +1,12 @@
 import pandas as pd
-data=pd.read_csv("data.csv")
-pd.options.display.max_rows=3
-pd.options.display.max_columns=3
-# print(data.to_string())
-# print(data.loc[135])
+df=pd.read_csv("data.csv")
+# pd.options.display.max_rows=3
+# pd.options.display.max_columns=3
+# print(df.to_string())
+# print(df.loc[135])
 # for i in range(12):
-#     print(data.loc[i])
-# short=data[data["Height"]>1]
+#     print(df.loc[i])
+# short=df[df["Height"]>1]
 # print(short.to_string())
-print(data)
+# print(df.head(8))
+print(df.info())
