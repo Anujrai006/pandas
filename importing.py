68a4616685ab615268cef821576fd46b5f534a81
Anuj Rai <anujxilling253745@gmail.com>
2026-03-23 05:06:43 +0545

    new changes

--- importing.py
+++ importing.py
@@ -1,8 +1,11 @@
 import pandas as pd
 data=pd.read_csv("data.csv")
+pd.options.display.max_rows=3
+pd.options.display.max_columns=3
 # print(data.to_string())
 # print(data.loc[135])
 # for i in range(12):
 #     print(data.loc[i])
 # short=data[data["Height"]>1]
 # print(short.to_string())
+print(data)
