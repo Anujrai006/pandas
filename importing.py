05918826d7bf240bdaac3d4956ab1a2357a774b1
Anuj Rai <anujxilling253745@gmail.com>
2026-03-23 04:59:55 +0545

    new files uploaded

---
+++ importing.py
@@ -0,0 +1,8 @@
+import pandas as pd
+data=pd.read_csv("data.csv")
+# print(data.to_string())
+# print(data.loc[135])
+# for i in range(12):
+#     print(data.loc[i])
+# short=data[data["Height"]>1]
+# print(short.to_string())
