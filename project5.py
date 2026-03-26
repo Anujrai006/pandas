d9c05958dabd0f6f75ef6e67eaaac8a73286d659
Anuj Rai <anujxilling253745@gmail.com>
2026-03-27 05:43:42 +0545

    new changes made

--- project5.py
+++ project5.py
@@ -1,30 +1,38 @@
+
 import pandas as pd
 num=int(input("Enter the number of students:"))
 Students=[]
-students={}
+stored_names=[]
 for i in range(num):
     name=input("Enter the name:")
-    physics=str(input("Enter marks  for physics"))
-    chemistry=str(input("Enter marks for chemistry"))
-    maths=str(input("Enter marks for maths"))
-    total=int(physics)+int(chemistry)+int(maths)
-    total=str(total)
+    if name in stored_names:
+        print(f"{name} already exists")
+        continue
+    physics=int(input("Enter marks  for physics"))
+    chemistry=int(input("Enter marks for chemistry"))
+    maths=int(input("Enter marks for maths"))
+    total=physics+chemistry+maths
+    # total=str(total)
     
-    # students={
-    #     "name":name,
-    #     "physics":physics,
-    #     "chemistry":chemistry,
-    #     "maths":maths,
-    #     "total":total
-    # }
-    with open("data5.csv" ,"a") as f:
-        f.write(f"{name},")
-        f.write(f"{physics},")
-        f.write(f"{chemistry},")
-        f.write(f"{maths},")
-        f.write(f"{total}, \n")
-model=["Name","Physics","Chemistry","Maths","Total"]
-df=pd.read_csv("data5.csv",names=model)
+  
+    Students.append({
+        "name":name,
+        "physics":physics,
+        "chemistry":chemistry,
+        "maths":maths,
+        "total":total
+    })
+    print(f"{name} Saved succesfully 🎉")
+    stored_names.append(name)
+    # with open("data5.csv" ,"a") as f: code for permanent 
+    #     f.write(f"{name},")
+    #     f.write(f"{physics},")
+    #     f.write(f"{chemistry},")
+    #     f.write(f"{maths},")
+    #     f.write(f"{total},\n")
+# model=["Name","Physics","Chemistry","Maths","Total"]
+df=pd.DataFrame(Students)
+df.to_csv("data5.csv",index=False)
 print(df)
 
 
