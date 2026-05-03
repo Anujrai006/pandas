3321526df0ea8b65c342046d24e7ff10265e2611
Anuj Rai <anujxilling253745@gmail.com>
2026-05-03 13:54:31 +0545

    new concepts done

--- filtering1.py
+++ filtering1.py
@@ -1,7 +1,10 @@
 import pandas as pd
 Studnets={
     "Name":["anuj","bibash","chandra","dipak"],
-    "Age":[12,13,14,19]
+    "Age":[12,13,14,19],
+    "Physics":[10,11,23,22],
+    "Chemistry":[21,22,12,29]
+
 }
 df=pd.DataFrame(Studnets,index=['a' ,'b','c','d'])
 child=df["Age"]<=14
@@ -10,5 +13,9 @@ child=df["Age"]<=14
 # querry=df.query("Age < 14")
 # print(querry)
 (df.loc["a","Age"])+=3
-df.loc[df["Age"]< 15,"result"]="fail"
+df["Percentage"]=((df["Physics"]+df["Chemistry"])/60)*100
+df.loc[df["Percentage"]< 60,"result"]="fail"
+df.rename(columns={"result":"results"},inplace=True)
+# df.drop("Age",axis=1,inplace=True)
+df["Age"].replace(15,13,inplace=True)
 print(df)
