6aad3d690a0e254641edfbc333dd1fad709d10b4
Anuj Rai <anujxilling253745@gmail.com>
2026-05-07 05:18:31 +0545

    new concepts

--- viewing.py
+++ viewing.py
@@ -1,3 +1,13 @@
+# import pandas as pd
+# df=pd.read_csv("data.csv")
+# # print(df[["Name"]].head(10))
+# print(df.loc["Name"])\
 import pandas as pd
-df=pd.read_csv("data.csv")
-print(df[["Name"]].head(10))
+
+# df = pd.DataFrame({
+#     'A': [10, 20, 30],
+#     'B': [40, 50, 60]
+# }, index=['x', 'y', 'z'])
+
+# print(df.loc['x'] )       # row with label 'x'
+# df.loc['x', 'A']   # value at row 'x', column 'A'
