a1aa1b0fb13e973dcd01993bd993bf183b12bb50
Anuj Rai <anujxilling253745@gmail.com>
2026-05-10 06:36:57 +0545

    handling wrong data

---
+++ wrong.py
@@ -0,0 +1,10 @@
+import pandas as pd
+df=pd.read_csv("wrong.csv")
+df.loc[7,"Duration"]=45
+# for x in df.index:
+#     if df.loc[x,"Duration"]>120:
+#         df.loc[x,"Duration"]=120
+for x in df.index:
+    if df.loc[x,"Duration"]>120:
+        df.drop(x,inplace=True)
+print(df)
