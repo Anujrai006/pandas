cdaf323875b657567d418ed0ca4b8a284ece5f9d
Anuj Rai <anujxilling253745@gmail.com>
2026-05-15 05:58:32 +0545

    joining in pandas

---
+++ joining.py
@@ -0,0 +1,14 @@
+import pandas as pd
+df1=pd.DataFrame(
+    {
+        "Name":["Anuj","Ram","Hari"],
+        "class":[11,12,10]
+    },index=[1,2,3]
+)
+df2=pd.DataFrame(
+    {
+        "Age":[17,18,15]
+    },index=[2,3,4]
+)
+df=df1.join(df2)
+print(df)
