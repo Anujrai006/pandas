791984c8c24bc0b106d398d9c297e58d4e7f103c
Anuj Rai <anujxilling253745@gmail.com>
2026-06-02 20:32:00 +0545

    grade added

--- mega.py
+++ mega.py
@@ -6,5 +6,13 @@ df["Attendance"]=df["Attendance"].fillna(df["Attendance"].median())
 df["Total"]=df["English"]+df["Science"]+df["Math"]+df["Nepali"]+df["Computer"]
 df["Total"]=df["Total"].astype(int)
 df["Percentage"]=(df["Total"]/500)*100 
-df["Percentage"]=df["Percentage"].astype(str) + "%"
+# df["Percentage"]=df["Percentage"].astype(str) + "%"
+df.loc[(df["Percentage"]>=90) ,"Grade"]="A+"
+df.loc[(df["Percentage"]>=80) & (df["Percentage"]<90),"Grade"]="A"
+df.loc[(df["Percentage"]>=70) & (df["Percentage"]<80),"Grade"]="B+"
+df.loc[(df["Percentage"]>=60) & (df["Percentage"]<70),"Grade"]="B"
+df.loc[(df["Percentage"]>=50) & (df["Percentage"]<60),"Grade"]="C+"
+df.loc[(df["Percentage"]>=40) & (df["Percentage"]<50),"Grade"]="C"
+df.loc[df["Percentage"]<40,"Grade"]="Fail"
+
 print(df)
