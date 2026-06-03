e0d2a9e1a779afc940e0aac502b359768c5410d6
Anuj Rai <anujxilling253745@gmail.com>
2026-06-03 08:58:42 +0545

    topper defined, lopper defined,average % by city and gender as welll as highest scoring city

--- mega.py
+++ mega.py
@@ -14,5 +14,17 @@ df.loc[(df["Percentage"]>=60) & (df["Percentage"]<70),"Grade"]="B"
 df.loc[(df["Percentage"]>=50) & (df["Percentage"]<60),"Grade"]="C+"
 df.loc[(df["Percentage"]>=40) & (df["Percentage"]<50),"Grade"]="C"
 df.loc[df["Percentage"]<40,"Grade"]="Fail"
-
-print(df)
+df.loc[df["Attendance"]>=75,"Status"]="Pass"
+df.loc[df["Attendance"]<75,"Status"]="Fail"
+top_idx=df["Percentage"].idxmax()
+print("Result of class 11:")
+print(f"Topper is : {df.loc[top_idx,"Name"]} with {df.loc[top_idx,"Percentage"]}%")
+low_idx=df["Percentage"].idxmin()
+print(f"lopper is : {df.loc[low_idx,"Name"]} with {df.loc[low_idx,"Percentage"]}%")
+print(df["Grade"].value_counts())
+print("average percentage by gender:")
+print(df.groupby("Gender")["Percentage"].mean())
+avg_city=df.groupby("City")["Percentage"].mean()
+# city_max=df[avg_city,"Percentage"].idxmax()
+city_max=avg_city.idxmax()
+print(f"Highest scoring city is {city_max}")
