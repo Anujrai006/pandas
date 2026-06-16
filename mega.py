ccfa559b1f5aa69d57e753c97c11ec1e043fae02
Anuj Rai <anujxilling253745@gmail.com>
2026-06-16 10:08:37 +0545

    new function to cal rank is added

--- mega.py
+++ mega.py
@@ -71,5 +71,8 @@ df.to_csv("Clean_data.csv",index=False)
 failed.to_csv("failed_stds.csv",index=False)
 avg_city.to_csv("city report",index=False)
 sorete_by_per=df.sort_values(by="Percentage",ascending=False)
-for i in range(1,len(df)):
-   df["Rank"]=df.loc[sorete_by_per[i]]
+df["Rank"]=df["Percentage"].rank(ascending=False)
+def rank(x):
+   name=str(input("Enter student name:"))
+   print(f" Rank of {name} is :{x.loc[x["Name"]==name,"Rank"].item()}")
+rank(df)
